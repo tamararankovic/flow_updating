d61bf27c67d4da140d72a241706d5e50a13a858e
Tamara Ranković <rankovictamaraa@gmail.com>
2025-09-10 11:41:00 +0200

    export tate

--- main.go
+++ main.go
@@ -266,7 +266,14 @@ func main() {
 	r.HandleFunc("POST /metrics", node.setMetricsHandler)
 	log.Println("Metrics server listening on :9200/metrics")
 
-	log.Fatal(http.ListenAndServe(strings.Split(os.Getenv("LISTEN_ADDR"), ":")[0]+":9200", r))
+	go func() {
+		log.Fatal(http.ListenAndServe(strings.Split(os.Getenv("LISTEN_ADDR"), ":")[0]+":9200", r))
+	}()
+
+	r = http.NewServeMux()
+	r.HandleFunc("GET /state", node.StateHandler)
+	log.Println("State server listening on :5001/metrics")
+	log.Fatal(http.ListenAndServe(strings.Split(os.Getenv("LISTEN_ADDR"), ":")[0]+":5001", r))
 }
 
 func sum(m map[string]float64) float64 {
